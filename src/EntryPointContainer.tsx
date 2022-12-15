6a2f63c99aed8f4a82c3837e47db8a556104cc28
Dmitry Chukhlyaev <chukhlyaev.dmitry@mail.ru>
2022-12-15 10:13:46 +0000

    Update EntryPointContainer.tsx

--- src/EntryPointContainer.tsx
+++ src/EntryPointContainer.tsx
@@ -2,19 +2,20 @@ import { PropsWithRef, useEffect, useState } from "react";
 import { EntryPoint } from "./types";
 
 export const EntryPointContainer = <Variables, Props extends {}>({
-  entrypoint: {component: Component, fetch, variables},
-}: {entrypoint: EntryPoint<Variables, Props>}) => {
+  entrypoint: { component: Component, fetch, variables },
+}: {
+  entrypoint: EntryPoint<Variables, Props>;
+}) => {
   const [props, setProps] = useState<Props | null>(null);
 
   useEffect(() => {
-    fetch(variables)
-          .then(props => {
-              setProps(props);
-          });
+    fetch(variables).then((props) => {
+      setProps(props);
+    });
   }, [fetch, variables]);
 
-  if (props === null) return null;
+  if (props === null) return <div>"loading..."</div>;
 
   // TODO: check
-  return <Component {...(props as React.Attributes & PropsWithRef<Props>)}/>;
+  return <Component {...(props as React.Attributes & PropsWithRef<Props>)} />;
 };
